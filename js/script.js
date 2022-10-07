acd119fd862c5bfd6879bba76836ad0cb511ea94
GtDavide01 <davidefiorini01@gmail.com>
2022-10-07 14:03:23 +0200

    esercizio con form semplice fatto

--- js/script.js
+++ js/script.js
@@ -10,3 +10,68 @@ Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente
 MILESTONE 2 (BONUS):
 Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
 Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).*/
+
+
+
+//prendo eta , kilometri percorsi dal passeggero  e bottone 
+const userKm = document.getElementById("userkm");
+console.log(userKm);
+
+const userAge = document.getElementById("userage");
+console.log(userAge);
+
+const btnInvia = document.getElementById("submit");
+console.log(btnInvia);
+
+const pKm = document.querySelector(".user-km-insert")
+console.log(pKm);
+
+const pAge = document.querySelector(".user-age-insert")
+console.log(pAge);
+
+const result = document.getElementById("result");
+
+btnInvia.addEventListener("click" , function (){
+    // trovo il valore di km che ha inserito l'utente 
+    const userKmInput = userKm.value;  
+    console.log(userKmInput);
+
+    //trovo il valore dell'età che ha inserito l'utente 
+    const userAgeInput = userAge.value;
+    console.log(userAgeInput);
+
+    //Restituisco in output il valore di km ed età del passaggero
+    pKm.innerHTML=`I kilometri che vuole percorrere il passeggero sono ${userKmInput}`;
+    pAge.innerHTML=`L'età del passeggero è di ${userAgeInput}`+ " anni";
+    //Azzero i valori una volta inseriti i dati 
+    userKm.value="";
+    userAge.value="";
+
+    const priceBase = 0.21 * userKmInput ;
+    console.log(priceBase);
+    //Calcolo sconto 
+    const discount20 = priceBase * 0.2;
+    const discount40 = priceBase * 0.4;
+    
+    let message = "";
+    let priceBaseDiscount  ; 
+    if (userAgeInput <= 18 ){
+        priceBaseDiscount = priceBase - discount20;
+        console.log(priceBaseDiscount);
+        message = "Il prezzo del suo biglietto è scontato del 20% ed è " + priceBaseDiscount.toFixed(2) + "€";
+        console.log(message);
+    } 
+    // if se l'utente ha più di 85 anni 
+    else if (userAgeInput >= 65 ){
+        priceBaseDiscount = priceBase - discount40;
+        console.log(priceBaseDiscount); 
+        message = "Il prezzo del suo biglietto è scontato del 40% ed è " + priceBaseDiscount.toFixed(2) + "€";
+        console.log(message);
+    }
+    // altrimenti stampa prezzo non scontato 
+    else {
+        message = "Il prezzo del suo biglietto non è scontato ed è  " + priceBase.toFixed(2) + "€";
+        console.log(message);
+    }
+    result.innerHTML=message;
+})
